7aac5b67d9696fe405658af634d953dcd95b2481
Md. Rubayet Islam <77844439+feedofmri@users.noreply.github.com>
2025-02-02 02:55:17 +0600

    remove toggle button

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -55,19 +55,6 @@ const Navbar = () => {
           </Link>
         </div>
 
-        {/* Toggle Button */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
         {/* Navbar Links */}
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mx-auto" style={{ gap: "1.5rem" }}>

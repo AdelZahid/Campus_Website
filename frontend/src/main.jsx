bbdab88060448efbfbd320a2307c5aca1dbb9124
AdelZahid <zahid.cse.20220204057@aust.edu>
2025-02-01 21:47:37 +0600

    Login_page>using vanilacss

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./componetns/App.jsx";
+import Login from "./componetns/Lgoin.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Login />
+  </StrictMode>
+);

8bb22a5a42663795602caf804e7017120ed80188
Md. Rubayet Islam <77844439+feedofmri@users.noreply.github.com>
2025-02-16 02:41:25 +0600

    fggbdbd

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Home from "./pages/HomePage/HomePage.jsx";
 import Login from "./pages/LoginPage/LoginPage.jsx";
+import Library from "./pages/LibraryPage/LibraryPage.jsx";
+import Messages from "./pages/Chatpage/Pages/home.jsx"
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Login />
+    <Messages />
   </StrictMode>
 );

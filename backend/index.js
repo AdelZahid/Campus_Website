76a41576217b52fa33f83244edee6208dd6fbd05
AdelZahid <zahid.cse.20220204057@aust.edu>
2025-03-02 02:19:40 +0600

    mongodb server is connected

--- backend/index.js
+++ backend/index.js
@@ -1,69 +1,33 @@
-import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
-import createTonkenAndSaveCookie from "../jwt/generateToken.js";
-export const signup = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ message: "User already registered" });
-    }
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRoutes from "./routes/user.routes.js";
+const app = express();
+dotenv.config();
 
-    // Hash password
-    const hassedPassword = await bcrypt.hash(password, 10);
-    const newUser = await new User({
-      username,
-      email,
-      password: hassedPassword,
-    });
-    await newUser.save();
-    if (newUser) {
-      createTonkenAndSaveCookie(newUser._id, res);
-      res
-        .status(201)
-        .json({ message: "User registered successfully", newUser });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Server error" });
+const PORT = process.env.PORT || 5173;
+const URI = process.env.MONGO_URI;
+
+app.use(express.json());
+
+try {
+  console.log("MONGODB_URI:", URI);
+  if (!URI) {
+    throw new Error("MongoDB URI is required");
   }
-};
+  mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("MongoDB Connected");
+} catch (error) {
+  console.log("Failed to connect", error);
+}
 
-export const login = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: "User not Found" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid Password" });
-    } else if (user && isMatch) {
-      createTonkenAndSaveCookie(user._id, res);
-      res.status(200).json({
-        message: "User logged in successfully",
-        user: {
-          _id: user._id,
-          username: user.username,
-          email: user.email,
-        },
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Server error" });
-  }
-};
-
-export const logout = async (req, res) => {
-  try {
-    res.clearCookie("jwt");
-    res.status(200).json({ message: "User Logout successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Server error" });
-  }
-};
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+app.use("/user", userRoutes);
+app.listen(PORT, () => {
+  console.log(`Example app listening on ${PORT}`);
+});

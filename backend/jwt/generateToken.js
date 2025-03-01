ce227d46a59b3ed3b04cbf191caab8bc0c9e14cc
AdelZahid <zahid.cse.20220204057@aust.edu>
2025-03-02 01:55:22 +0600

    userRoute and Token is added

---
+++ backend/jwt/generateToken.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+const createTonkenAndSaveCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_TOKEN, {
+    expiresIn: "10d",
+  });
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+  });
+};
+
+export default createTonkenAndSaveCookie;
